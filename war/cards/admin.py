86a6856f7da4acae026fc94b4b434f819107c640
Peter <pwgraham91@gmail.com>
2014-10-16 14:50:03 -0700

    admin selenium is good, minor update to admin file to display other models

--- war/cards/admin.py
+++ war/cards/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from cards.models import Card
+from cards.models import Card, Player, WarGame
 
 admin.site.register(Card)
+admin.site.register(Player)
+admin.site.register(WarGame)
